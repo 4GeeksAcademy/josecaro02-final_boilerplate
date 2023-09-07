9a2b96099812f1603d37ec4a40ca7f19748cc5a5
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2023-09-07 15:39:32 +0000

    Change debud in app.py

--- src/app.py
+++ src/app.py
@@ -89,4 +89,4 @@ def send_email():
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    app.run(host='0.0.0.0', port=PORT, debug=False)
